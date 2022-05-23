5a66f505552d7c728088359d32fa8541200a2609
Zewas <shumahershik@gmail.com>
2022-05-23 22:51:18 +0300

    temperature convertation functions

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    return (f - 32) / (9 / 5);
+    return (f - 32.0) / (9.0 / 5.0);
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    return с * (9 / 5) + 32;
+    return с * (9.0 / 5.0) + 32.0;
 }
