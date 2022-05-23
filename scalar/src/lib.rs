2186a0e18d3f4ba296ee8281fd681b0c97301cdd
Zewas <shumahershik@gmail.com>
2022-05-23 22:15:58 +0300

    first try

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    a + b
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
+}
+
+pub fn quo(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+pub fn rem(a: i32, b: i32) -> i32 {
+    a % b
+}
