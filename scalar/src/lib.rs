5dd145e56775be74665bd787d6e823680da9f43b
Zewas <shumahershik@gmail.com>
2022-05-23 22:22:26 +0300

    hot fix

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -10,10 +10,10 @@ pub fn pro(a: i8, b: i8) -> i8 {
     a * b
 }
 
-pub fn quo(a: i32, b: i32) -> i32 {
+pub fn quo(a: f32, b: f32) -> f32 {
     a / b
 }
 
-pub fn rem(a: i32, b: i32) -> i32 {
+pub fn rem(a: f32, b: f32) -> f32 {
     a % b
 }
