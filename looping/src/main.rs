4b801eec2a3fde86cca8c48b03906dd262de4b65
Zewas <shumahershik@gmail.com>
2022-05-24 02:36:37 +0300

    riddle program

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut i = 1;
+    let num = loop {
+        let mut input = String::new();
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+        io::stdin().read_line(&mut input)?;
+        if input.trim() == "The letter e" {
+            break i;
+        } else {
+            i += 1
+        }
+    };
+    println!("{}", num);
+
+    Ok(())
 }
