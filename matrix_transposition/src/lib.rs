ebcd43a386cfb6b94a5aef8cce5687e5e0ddf0b3
Zewas <shumahershik@gmail.com>
2022-05-24 14:56:11 +0300

    matrix transpose function

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    let (a, b) = m.0;
+    let (c, d) = m.1;
+    Matrix((a, c), (b, d))
+}
