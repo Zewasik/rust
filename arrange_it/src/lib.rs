5e795818487f5240a446bc522b5cd51734b49c0a
Zewas <shumahershik@gmail.com>
2022-05-25 22:21:40 +0300

    arrange words function

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let nbrs: Vec<&str> = phrase.matches(char::is_numeric).collect();
+    let seq: Vec<&str> = phrase.matches(char::is_numeric).collect();
     let temp_str = &phrase.replace(char::is_numeric, "");
     let mut words: Vec<&str> = temp_str.split_whitespace().collect();
-    for (i, value) in nbrs.iter().enumerate() {
+    for (i, value) in seq.iter().enumerate() {
         let strs: Vec<&str> = temp_str.split_whitespace().collect();
         words[value.parse::<usize>().unwrap() - 1] = strs[i];
     }
