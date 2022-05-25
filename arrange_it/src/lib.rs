52f38ff6d6f6ef337d70dee6704bb63f2eda4b7f
Zewas <shumahershik@gmail.com>
2022-05-25 20:01:13 +0300

    arrange words function

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,21 +1,23 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let words: Vec<&str> = phrase.split(" ").collect();
-    let mut correct_phrase: Vec<String> = vec!["".to_string(); words.len() - 1];
-    let mut index = 0;
-    for &word in &words {
-        let mut temp_str = "".to_string();
-        for ch in word.chars() {
-            if ch.is_ascii_digit() {
-                index = ch as usize - 48;
-            } else {
-                temp_str += &ch.to_string();
+    let ans = {
+        let words: Vec<&str> = phrase.split(" ").collect();
+        let mut correct_phrase: Vec<String> = vec!["".to_string(); words.len() - 1];
+        let mut index = 0;
+        for &word in &words {
+            let mut temp_str = "".to_string();
+            for ch in word.chars() {
+                if ch.is_ascii_digit() {
+                    index = ch as usize - 48;
+                } else {
+                    temp_str += &ch.to_string();
+                }
             }
+            if index < correct_phrase.len() {
+                correct_phrase.remove(&index - 1);
+            }
+            correct_phrase.insert(&index - 1, temp_str);
         }
-        if index < correct_phrase.len() {
-            correct_phrase.remove(&index - 1);
-        }
-        correct_phrase.insert(&index - 1, temp_str);
-    }
-
-    return correct_phrase.join(" ");
+        correct_phrase
+    };
+    return ans.join(" ");
 }
