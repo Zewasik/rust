a5839d5c797ea2135aa91bde7154dc9dcc8196d2
Zewas <shumahershik@gmail.com>
2022-06-01 18:32:00 +0300

    boxing errors:(

--- boxing_todo/src/err.rs
+++ boxing_todo/src/err.rs
@@ -4,31 +4,35 @@ use std::fmt::{self, write};
 
 #[derive(Debug)]
 pub enum ParseErr {
-    Malformed,
+    Empty,
+    Malformed(Box<dyn Error>),
 }
 
 // required by error trait
 impl Display for ParseErr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write(f, format_args!("Fail to parse todo"))
+        write(f, format_args!("Fail to parse todo "))
     }
 }
 
 #[derive(Debug)]
 pub struct ReadErr {
-    child_err: Box<dyn Error>,
+    pub child_err: Box<dyn Error>,
 }
 
 // required by error trait
 impl Display for ReadErr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write(f, format_args!("Fail to read todo file"))
+        write(f, format_args!("Fail to read todo file "))
     }
 }
 
 impl Error for ParseErr {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&ParseErr::Malformed)
+        match self {
+            ParseErr::Empty => None,
+            ParseErr::Malformed(err) => Some(err.as_ref()),
+        }
     }
 }
 
--- boxing_todo/src/lib.rs
+++ boxing_todo/src/lib.rs
@@ -4,6 +4,7 @@ use err::{ParseErr, ReadErr};
 
 pub use json::{parse, stringify};
 pub use std::error::Error;
+use std::fs;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Task {
@@ -20,14 +21,27 @@ pub struct TodoList {
 
 impl TodoList {
     pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
-        let raw_json = match parse(path) {
+        let str = match fs::read_to_string(path) {
             Ok(result) => result,
-            Err(err) => return Err(Box::new(err)),
+            Err(err) => {
+                return Err(Box::new(ReadErr {
+                    child_err: Box::new(err),
+                }))
+            }
+        };
+
+        let raw_json = match parse(&str) {
+            Ok(result) => result,
+            Err(err) => return Err(Box::new(ParseErr::Malformed(Box::new(err)))),
         };
 
         let title = raw_json["title"].to_string();
         let mut temp = Vec::new();
         let tasks = raw_json["tasks"].members();
+
+        if tasks.clone().count() == 0 {
+            return Err(Box::new(ParseErr::Empty));
+        }
 
         for obj in tasks {
             let mut temp_obj = obj.entries();
