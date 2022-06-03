b63e3ed16b621bdc59c078d505138860cc2acd7b
Zewas <shumahershik@gmail.com>
2022-06-03 22:20:58 +0300

    ordinary numbers

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn num_to_ordinal(x: u32) -> String {
     match x % 10 {
-        1 => x.to_string() + "st",
-        2 => x.to_string() + "nd",
-        3 => x.to_string() + "rd",
+        1 => x.to_string() + if x == 11 { "th" } else { "st" },
+        2 => x.to_string() + if x == 12 { "th" } else { "nd" },
+        3 => x.to_string() + if x == 13 { "th" } else { "rd" },
         _ => x.to_string() + "th",
     }
 }
