2a998f734d71ff89683da813537dda7664c78e49
Zewas <shumahershik@gmail.com>
2022-06-03 22:33:38 +0300

    ordinary numbers

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn num_to_ordinal(x: u32) -> String {
     match x % 10 {
-        1 => x.to_string() + if x == 11 { "th" } else { "st" },
-        2 => x.to_string() + if x == 12 { "th" } else { "nd" },
-        3 => x.to_string() + if x == 13 { "th" } else { "rd" },
+        1 if x != 11 => x.to_string() + "st",
+        2 if x != 12 => x.to_string() + "nd",
+        3 if x != 13 => x.to_string() + "rd",
         _ => x.to_string() + "th",
     }
 }
--- ordinal/src/main.rs
+++ ordinal/src/main.rs
@@ -2,7 +2,7 @@ use ordinal::*;
 
 fn main() {
     println!("{}", num_to_ordinal(1));
-    println!("{}", num_to_ordinal(22));
+    println!("{}", num_to_ordinal(12));
     println!("{}", num_to_ordinal(43));
     println!("{}", num_to_ordinal(47));
 }
