8b2adfa6282b4cf0e3c9c9364bb0c0d07ff3c7b5
Zewas <shumahershik@gmail.com>
2022-06-05 20:05:05 +0300

    expected update

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,21 +1,19 @@
 pub mod edit_distance;
 use edit_distance::edit_distance;
+
 pub fn expected_variable(to_compare: &str, expected: &str) -> Option<String> {
-    if to_compare.contains(' ')
-        || to_compare.contains('-')
-        || expected.contains(' ')
-        || expected.contains('-')
+    if to_compare.contains(|ch| ch == ' ' || ch == '-')
+        || expected.contains(|ch| ch == ' ' || ch == '-')
     {
         return None;
     };
-    let a = to_compare.to_lowercase();
-    let b = expected.to_lowercase();
-    let dist = edit_distance(&a, &b);
+
+    let dist = edit_distance(&to_compare.to_lowercase(), &expected.to_lowercase());
     let ans = ((1.0 - (dist as f64 / expected.len() as f64)) * 100.0).round() as i32;
 
     if ans < 50 {
         return None;
     };
 
-    return Some(ans.to_string() + "%");
+    Some(ans.to_string() + "%")
 }
