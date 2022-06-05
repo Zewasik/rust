fa0d079302bba6931ae2130d0498e15afa3ce56c
Zewas <shumahershik@gmail.com>
2022-06-05 08:44:46 +0300

    pig latin converter

---
+++ pig_latin/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn pig_latin(text: &str) -> String {
+    let is_vowels = |ch: char| ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+
+    if text.len() == 0 {
+        return text.to_owned();
+    }
+
+    if !is_vowels(text[0..1].chars().next().unwrap()) {
+        if text.len() >= 3 {
+            if &text[1..3] == "qu" {
+                return text[3..].to_owned() + &text[0..3] + "ay";
+            }
+        }
+        match text.find(is_vowels) {
+            Some(i) => return text[i..].to_owned() + &text[0..i] + "ay",
+            None => return text.to_owned() + "ay",
+        }
+    }
+
+    text.to_owned() + "ay"
+}
