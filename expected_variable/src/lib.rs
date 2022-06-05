5915a6f78c911ed25894f895634551bf6e48cc7e
Zewas <shumahershik@gmail.com>
2022-06-05 20:01:49 +0300

    expected update

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -10,13 +10,12 @@ pub fn expected_variable(to_compare: &str, expected: &str) -> Option<String> {
     };
     let a = to_compare.to_lowercase();
     let b = expected.to_lowercase();
-    let test = edit_distance(&a, &b);
-    let temp = ((1.0 - (test as f64 / expected.len() as f64)) * 100.0).round() as i32;
+    let dist = edit_distance(&a, &b);
+    let ans = ((1.0 - (dist as f64 / expected.len() as f64)) * 100.0).round() as i32;
 
-    if temp < 50 {
+    if ans < 50 {
         return None;
     };
-    let temp_str = temp.to_string();
-    let res: String = temp_str + "%";
-    return Some(res);
+
+    return Some(ans.to_string() + "%");
 }
