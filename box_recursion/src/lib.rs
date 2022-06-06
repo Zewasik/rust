bbab42e502635719e513a2c397931aded385a1b6
Zewas <shumahershik@gmail.com>
2022-06-06 20:22:05 +0300

    recursion type

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+// pub enum Link {
+//     Some(Box<Worker>, Box<Link>),
+//     None,
+// }
+
+pub type Link = Option<Box<Worker>>;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Worker {
+    pub worker_type: String,
+    pub worker_name: String,
+    pub next_worker: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> WorkEnvironment {
+        WorkEnvironment { grade: Link::None }
+    }
+
+    pub fn add_worker(&mut self, t: String, name: String) {
+        self.grade = Link::Some(Box::new(Worker {
+            worker_type: t,
+            worker_name: name,
+            next_worker: self.grade.clone(),
+        }));
+    }
+
+    pub fn remove_worker(&mut self) -> Option<String> {
+        let name = match self.grade.clone() {
+            Link::Some(wok) => (wok.worker_name.clone(), wok.next_worker),
+            Link::None => return None,
+        };
+        self.grade = name.1;
+        return Some(name.0);
+    }
+
+    pub fn search_worker(&self) -> Option<(String, String)> {
+        match &self.grade {
+            Some(find) => Some((find.worker_name.clone(), find.worker_type.clone())),
+            None => None,
+        }
+    }
+}
---
+++ box_recursion/src/main.rs
@@ -0,0 +1,18 @@
+use box_recursion::*;
+
+fn main() {
+    let mut list = WorkEnvironment::new();
+    list.add_worker(String::from("CEO"), String::from("Marie"));
+    list.add_worker(String::from("Manager"), String::from("Monica"));
+    list.add_worker(String::from("Normal Worker"), String::from("Ana"));
+    list.add_worker(String::from("Normal Worker"), String::from("Alice"));
+    println!("{:?}", list);
+
+    println!("{:?}", list.search_worker());
+
+    list.remove_worker();
+    list.remove_worker();
+    list.remove_worker();
+    list.remove_worker();
+    println!("{:?}", list);
+}
