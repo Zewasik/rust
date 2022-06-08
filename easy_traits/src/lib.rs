c7406d2797d75c1846cf8e8c31026d0be1fe8fe9
Zewas <shumahershik@gmail.com>
2022-06-08 18:11:03 +0300

    one

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Clone)]
+struct StringValue {
+    value: String,
+}
+
+trait AppendStr {
+    fn append_str(self, new_str: String) -> Self;
+
+    fn append_number(self, new_number: f64) -> Self;
+
+    fn remove_punctuation_marks(self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(mut self, new_str: String) -> Self {
+        self.value.push_str(&new_str);
+        self
+    }
+
+    fn append_number(mut self, new_number: f64) -> Self {
+        self.value.push_str(&new_number.to_string());
+        self
+    }
+
+    fn remove_punctuation_marks(mut self) -> Self {
+        self.value = self
+            .value
+            .replace(|ch| ch == '.' || ch == ',' || ch == '?' || ch == '!', "");
+        self
+    }
+}
