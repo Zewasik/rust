8fe2757d21dff63db4f283b9a2755c9552375dc0
Zewas <shumahershik@gmail.com>
2022-06-09 22:16:52 +0300

    god pls help

--- lalgebra_scalar/src/lib.rs
+++ lalgebra_scalar/src/lib.rs
@@ -5,3 +5,75 @@ pub trait Scalar {
     fn zero() -> Self::Item;
     fn one() -> Self::Item;
 }
+
+impl Scalar for u32 {
+    type Item = u32;
+
+    fn zero() -> Self::Item {
+        0
+    }
+
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for u64 {
+    type Item = u64;
+
+    fn zero() -> Self::Item {
+        0
+    }
+
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+
+    fn zero() -> Self::Item {
+        0
+    }
+
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i64 {
+    type Item = u32;
+
+    fn zero() -> Self::Item {
+        0
+    }
+
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f32;
+
+    fn zero() -> Self::Item {
+        0.0
+    }
+
+    fn one() -> Self::Item {
+        1.0
+    }
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+
+    fn zero() -> Self::Item {
+        0.0
+    }
+
+    fn one() -> Self::Item {
+        1.0
+    }
+}
