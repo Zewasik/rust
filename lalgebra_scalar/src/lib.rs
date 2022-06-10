7d1df19c24be986128cf4c38524b7acb0a8f7404
Zewas <shumahershik@gmail.com>
2022-06-10 19:16:15 +0300

    god pls help

--- lalgebra_scalar/src/lib.rs
+++ lalgebra_scalar/src/lib.rs
@@ -1,77 +1,22 @@
-pub trait Scalar {
+use std::ops::{Add, Div, Mul, Sub};
+
+pub trait Scalar: Sized {
     type Item;
     fn zero() -> Self::Item;
     fn one() -> Self::Item;
 }
 
-impl Scalar for u32 {
-    type Item = u32;
+impl<T> Scalar for T
+where
+    T: From<u8> + PartialEq + Add + Sub + Mul + Div,
+{
+    type Item = T;
 
     fn zero() -> Self::Item {
-        0
+        T::from(0)
     }
 
     fn one() -> Self::Item {
-        1
-    }
-}
-
-impl Scalar for u64 {
-    type Item = u64;
-
-    fn zero() -> Self::Item {
-        0
-    }
-
-    fn one() -> Self::Item {
-        1
-    }
-}
-
-impl Scalar for i32 {
-    type Item = i32;
-
-    fn zero() -> Self::Item {
-        0
-    }
-
-    fn one() -> Self::Item {
-        1
-    }
-}
-
-impl Scalar for i64 {
-    type Item = i64;
-
-    fn zero() -> Self::Item {
-        0
-    }
-
-    fn one() -> Self::Item {
-        1
-    }
-}
-
-impl Scalar for f32 {
-    type Item = f32;
-
-    fn zero() -> Self::Item {
-        0.0
-    }
-
-    fn one() -> Self::Item {
-        1.0
-    }
-}
-
-impl Scalar for f64 {
-    type Item = f64;
-
-    fn zero() -> Self::Item {
-        0.0
-    }
-
-    fn one() -> Self::Item {
-        1.0
+        T::from(1)
     }
 }
