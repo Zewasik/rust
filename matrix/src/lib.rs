8e534155bf526c5d4fe62e060320e7a2dae70214
Zewas <shumahershik@gmail.com>
2022-06-13 16:59:34 +0300

    scalar matrix

---
+++ matrix/src/lib.rs
@@ -0,0 +1,38 @@
+use std::ops::{Add, Div, Mul, Sub};
+pub trait Scalar: Sized {
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+impl<T> Scalar for T
+where
+    T: From<u8> + PartialEq + Add + Sub + Mul + Div,
+{
+    type Item = T;
+    fn zero() -> Self::Item {
+        T::from(0)
+    }
+    fn one() -> Self::Item {
+        T::from(1)
+    }
+}
+
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Scalar<Item = T> + Clone> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::zero()]])
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut matrix = Self::zero(n, n);
+        for i in 0..n {
+            matrix.0[i][i] = T::one();
+        }
+        matrix
+    }
+}
