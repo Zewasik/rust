83a958f78af097cc127cd2ad0b9e18b2b7eccda7
Zewas <shumahershik@gmail.com>
2022-06-14 02:44:04 +0300

    lifetimes usage

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl Person<'_> {
+    pub fn new(name: &str) -> Person {
+        Person { name, age: 0 }
+    }
+}
