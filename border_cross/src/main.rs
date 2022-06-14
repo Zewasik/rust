8534eb11bd0da398d34e33296bb6ff439a180f7c
Zewas <shumahershik@gmail.com>
2022-06-14 16:36:09 +0300

    implement trait in function

---
+++ border_cross/src/main.rs
@@ -0,0 +1,20 @@
+use border_cross::*;
+
+fn main() {
+    let vehicles: Vec<&dyn Vehicle> = vec![
+        &Car {
+            plate_nbr: "A3D5C7",
+            model: "Model 3",
+            horse_power: 325,
+            year: 2010,
+        },
+        &Truck {
+            plate_nbr: "V3D5CT",
+            model: "Ranger",
+            horse_power: 325,
+            year: 2010,
+            load_tons: 40,
+        },
+    ];
+    println!("{:?}", all_models(vehicles));
+}
