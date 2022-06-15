18b8c52cab474971c4c8bf66b4b0a42b6923807d
Zewas <shumahershik@gmail.com>
2022-06-15 09:54:07 +0300

    calculate square of first fifty even elements

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,8 +1,3 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
-    let mut ans = vec![];
-    for i in (2..=100).step_by(2) {
-        ans.push(i * i);
-    }
-
-    ans
+    (2..=100).step_by(2).map(|i| i * i).collect()
 }
