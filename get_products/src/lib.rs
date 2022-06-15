68e1bcda1714dfcf890a09e14f92e71aa94d88bc
Zewas <shumahershik@gmail.com>
2022-06-15 15:41:39 +0300

    product of iter

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() < 2 {
+        return vec![];
+    }
     arr.iter()
         .map(|x| arr.iter().product::<usize>() / x)
         .collect()
