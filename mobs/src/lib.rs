db7d6c835f324c14228218c485ed806e254ef4e3
Zewas <shumahershik@gmail.com>
2022-06-17 00:23:47 +0300

    update lib file

--- mobs/src/lib.rs
+++ mobs/src/lib.rs
@@ -1,7 +1,4 @@
 mod mobs;
 
-pub use crate::mobs::boss::Boss;
-pub use crate::mobs::member::Member;
-pub use crate::mobs::member::Role::{Associate, Caporegime, Soldier, Underboss};
-pub use crate::mobs::*;
-pub use crate::mobs::{boss, member, Mob};
+pub use mobs::{boss, member, Mob};
+pub use mobs::{boss::*, member::*};
