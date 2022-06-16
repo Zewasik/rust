3f322c69aba45ab7526b18b2c012e340808a1713
Zewas <shumahershik@gmail.com>
2022-06-16 19:55:43 +0300

    array to hashmap

---
+++ slices_to_map/src/lib.rs
@@ -0,0 +1,17 @@
+use std::hash::Hash;
+use std::{cmp, collections::HashMap};
+
+pub fn slices_to_map<'a, T, U>(k: &'a [T], v: &'a [U]) -> HashMap<&'a T, &'a U>
+where
+    T: Hash + Eq,
+{
+    let len = cmp::min(k.len(), v.len());
+
+    let mut map: HashMap<&T, &U> = HashMap::new();
+
+    for i in 0..len {
+        map.insert(&k[i], &v[i]);
+    }
+
+    map
+}
