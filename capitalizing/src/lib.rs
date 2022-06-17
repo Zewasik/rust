5ec86e16f7e8f673772262f3c0ecbf48beb0aabf
Zewas <shumahershik@gmail.com>
2022-06-17 13:47:11 +0300

    optimizing code

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,41 +1,32 @@
 pub fn capitalize_first(input: &str) -> String {
-    if input == "" {
-        return "".to_string();
+    if input != "" {
+        input[..1].to_uppercase() + &input[1..]
+    } else {
+        "".to_string()
     }
-    return input[0..1].to_uppercase().to_string() + &input[1..];
 }
 
 pub fn title_case(input: &str) -> String {
-    let mut temp_str: String = String::from("");
-    let mut is_new = true;
-    for ch in input.chars() {
-        if is_new == true {
-            temp_str += &ch.to_uppercase().to_string();
-            is_new = false;
-        } else {
-            temp_str += &ch.to_string();
-        }
-
-        if ch.is_ascii_whitespace() == true {
-            is_new = true;
-        }
-    }
-
-    return temp_str;
+    input
+        .split_ascii_whitespace()
+        .map(|s| capitalize_first(s))
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 pub fn change_case(input: &str) -> String {
-    let mut temp_str: String = String::from("");
-
-    for ch in input.chars() {
-        if ch.is_ascii_lowercase() {
-            temp_str += &ch.to_uppercase().to_string();
-        } else if ch.is_ascii_uppercase() {
-            temp_str += &ch.to_lowercase().to_string();
-        } else {
-            temp_str += &ch.to_string();
-        }
-    }
-
-    return temp_str;
+    input
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphanumeric() {
+                if c.is_ascii_lowercase() {
+                    c.to_ascii_uppercase()
+                } else {
+                    c.to_ascii_lowercase()
+                }
+            } else {
+                c
+            }
+        })
+        .collect()
 }
